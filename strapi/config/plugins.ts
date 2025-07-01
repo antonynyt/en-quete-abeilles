c0bb26394f2331d49a9a416f479fdb9885648844
Antony Neyret <antony.neyret@gmail.com>
2025-07-01 16:18:36 +0200

    feat: add strapi-plugin-qr-code

--- strapi/config/plugins.ts
+++ strapi/config/plugins.ts
@@ -1,1 +1,18 @@
-export default () => ({});
+// config/plugins.ts
+import type { Config as QRCodeConfig } from "strapi-plugin-qr-code/dist/server/src/config";
+
+export default () => ({
+    "qr-code": {
+        enabled: true,
+        config: {
+            contentTypes: [
+                {
+                    uid: "api::task.task",
+                    computeValue: (uid, status, document) => {
+                        return `/t/${document.documentId}`;
+                    },
+                },
+            ],
+        } satisfies QRCodeConfig,
+    },
+});
