5d98e9122657baf91aab5273ef26826165c502b2
Antony Neyret <antony.neyret@gmail.com>
2025-07-21 14:12:29 +0200

    feat(video-player): create VideoPlayerControls component

---
+++ borne/frontend/src/components/VideoPlayerControls.vue
@@ -0,0 +1,84 @@
+<script setup>
+import IconPlay from './icons/IconPlay.vue'
+
+</script>
+
+<template>
+    <div class="player__controls">
+        <button class="player__button button" title="Play (space)">
+            <IconPlay />
+        </button>
+        <div class="progress">
+            <div class="progress__filled"></div>
+        </div>
+        <div class="player__timing">
+            <span class="player__current-time">0:00</span>
+            <span class="player__duration">0:00</span>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.player__controls {
+    display: flex;
+    align-items: center;
+    gap: var(--spacing-md);
+    background: var(--color-beige-light);
+    padding: var(--spacing-md);
+    border-radius: var(--radius-md);
+    border: var(--border-size) solid var(--color-beige);
+}
+
+.player__button {
+    color: var(--color-brown);
+    display: flex;
+    width: auto;
+}
+
+.progress {
+    display: flex;
+    height: 1em;
+    background: var(--color-beige);
+    cursor: pointer;
+    border-radius: 1em;
+    flex-grow: 1;
+}
+
+.progress__filled {
+    width: 50%;
+    background: var(--color-brown);
+    flex: 0;
+    flex-basis: 0%;
+    position: relative;
+    border-radius: 1em;
+    display: flex;
+    align-items: center;
+}
+
+.progress__filled::after {
+    content: '';
+    position: absolute;
+    height: 2em;
+    width: 1em;
+    border-radius: 1em;
+    right: -0.5em;
+    background: var(--color-brown);
+}
+
+.player__timing {
+    display: flex;
+    gap: var(--spacing-xs);
+}
+
+.player__current-time,
+.player__duration {
+    color: var(--color-brown);
+    font-size: var(--font-size-xs);
+}
+
+.player__duration::before {
+    content: '/';
+    margin-right: var(--spacing-xs);
+    color: var(--color-brown-light);
+}
+</style>
--- borne/frontend/src/views/SubjectDetailView.vue
+++ borne/frontend/src/views/SubjectDetailView.vue
@@ -1,44 +1,56 @@
 <script setup>
+import StrapiImage from '@/components/StrapiImage.vue';
+import VideoPlayerControls from '@/components/VideoPlayerControls.vue';
 import DetailLayout from '@/layouts/DetailLayout.vue';
-import BaseCardList from '@/components/BaseCardList.vue';
-import BaseCard from '@/components/BaseCard.vue';
-import IconPlay from '@/components/icons/IconPlay.vue';
 
 </script>
 
 <template>
-    <DetailLayout v-slot="{ subject }">
-        {{ subject }}
+    <DetailLayout v-slot="{ subject }" :blueToBottom="'20%'">
+        <div class="subject-detail">
+            <div class="subject-infos">
+                <StrapiImage v-if="subject.cover" :imageData="subject.cover" class="subject-image"
+                    :alt="subject.title" />
+                <div class="subject-content" v-if="subject.description">
+                    <h1>{{ subject.title }}</h1>
+                    <p>{{ subject.description }}</p>
+                </div>
+            </div>
+            <VideoPlayerControls class="controls" />
+        </div>
     </DetailLayout>
 </template>
 
 <style scoped>
-.subject-list {
-    height: 100%;
-    flex-grow: 1;
+.subject-detail {
     display: flex;
+    flex: 1;
     flex-direction: column;
-    justify-content: center;
-    gap: var(--spacing-lg);
-}
-
-.subject-list h2 {
-    text-align: center;
-    font-size: var(--font-size-sm);
-    font-weight: 500;
-}
-
-.list {
     height: 100%;
-    display: flex;
-    align-items: center;
+    justify-content: space-around;
 }
 
-.list :deep(h2) {
+p {
     font-size: var(--font-size-md);
 }
 
-.list :deep(.card) {
-    width: 25vw;
+.subject-infos {
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    gap: var(--spacing-md);
+    margin: 0 var(--spacing-md);
+}
+
+.subject-image {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    aspect-ratio: 16 / 9;
+    border: var(--border-size) solid var(--color-brown);
+    border-radius: var(--radius-sm);
+}
+
+.controls {
+    margin: 0 var(--spacing-md);
 }
 </style>
