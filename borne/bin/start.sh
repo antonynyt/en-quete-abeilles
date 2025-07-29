b8adac8f90c546e505d6ee98b26acd4d2df8e935
Antony Neyret <antony.neyret@gmail.com>
2025-07-29 22:32:08 +0200

    fix(start.sh): update launch-screen.sh script path

--- borne/bin/start.sh
+++ borne/bin/start.sh
@@ -4,13 +4,13 @@ set -e
 cd "$(dirname "$0")/.."
 docker compose up --force-recreate -d
 
-while ! docker compose ps | grep -q "running"; do
+while ! docker compose ps | grep -q "Up"; do
     sleep 2
     docker compose ps
 done
 
 if which chromium-browser &>/dev/null; then
-    ./borne/bin/launch-screens.sh
+    ./launch-screens.sh
 else
     exit 1
 fi
