8f0d8b056995f9c6354784521b9876670cdab1af
Antony Neyret <antony.neyret@gmail.com>
2025-07-30 19:12:08 +0200

    fix(borne): start scripts points to app wrapper

---
+++ borne/app-wrapper/README.md
@@ -0,0 +1,7 @@
+npm start
+npm run build -- --linux
+npx electron-builder --linux dir
+
+
+compiled file should be stored in
+`app-wrapper/dist/borne-wrapper/borne-wrapper`
--- borne/bin/start.sh
+++ borne/bin/start.sh
@@ -5,12 +5,14 @@ cd "$(dirname "$0")/.."
 docker compose up --force-recreate -d
 
 while ! docker compose ps | grep -q "Up"; do
-    sleep 2
-    docker compose ps
+   sleep 2
+   docker compose ps
 done
 
-if which chromium-browser &>/dev/null; then
-    bin/launch-screens.sh
+# start the app-wrapper
+if [ -f "app-wrapper/dist/borne-wrapper/borne-wrapper" ]; then
+    echo "Starting app-wrapper..."
+    ./app-wrapper/dist/borne-wrapper/borne-wrapper &
 else
-    exit 1
+    echo "App-wrapper not found, skipping..."
 fi
